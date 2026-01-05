4b6cb0c958634e0f57730ea2bfb39b1ad274009f
Roberto J. Vargas <rvargas@rv-solutions.net>
2026-01-05 13:30:24 -0500

    fix: Fixing prerender for form

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://almacenajes-minidepositos.com',
-  output: 'static',
+  output: 'server',
   adapter: vercel({
     webAnalytics: {
       enabled: true
